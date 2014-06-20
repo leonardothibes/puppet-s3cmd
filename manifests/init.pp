c8e2deefa32b2d853b998b09482735c83082c484
Leonardo Thibes <eu@leonardothibes.com>
2014-06-20 14:28:18 -0300

    implementando a classe principal de controle

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,22 @@
-# == Class: s3cmd
-#
-# Full description of class s3cmd here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { s3cmd:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2014 Your name here, unless otherwise noted.
-#
-class s3cmd {
+class s3cmd(
+	$ensure          = 'present',
+	$access_key      = 'ACCESS_KEY',
+	$secret_key      = 'SECRET_KEY',
+	$bucket_location = 'US',
+	$home_dir        = '/home',
+	$user,
+) inherits s3cmd::dependencies {
 
-
+	case $ensure {
+		'present': {
+			s3cmd::install {'s3cmd::install':} -> s3cmd::configure {$user:
+				access_key      => $access_key,
+				secret_key      => $secret_key,
+				bucket_location => $bucket_location,
+				home_dir        => $home_dir,
+			}
+		}
+		'absent': { s3cmd::purge {$user: home_dir => $home_dir} }
+		default : { fail("Unsupported option for \"ensure\" param: ${ensure}") }
+	}
 }
