715a4474b68b277e76060ea24b9a5ef4b4007426
Leonardo Thibes <eu@leonardothibes.com>
2014-06-20 14:28:06 -0300

    atualizando arquivo de testes

--- tests/init.pp
+++ tests/init.pp
@@ -9,4 +9,8 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-include s3cmd
+class {'s3cmd':
+	user       => 'vagrant',
+	access_key => 'ACCESS_KEY',
+	secret_key => 'SECRET_KEY',
+}
